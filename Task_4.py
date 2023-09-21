57cdf4fbb2f3403f350036cbb891d1c648b693bc
denis <zakablukov_77@inbox.ru>
2023-09-21 12:16:52 +0400

    INIT

--- Task_4.py
+++ Task_4.py
@@ -1,8 +1,17 @@
 import os
+import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
-
 TOKEN = os.getenv('TOKEN')
 
-print(f'TOKEN - {TOKEN}')
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+    response = "Ба! Знакомые все лица!"
+    bot.send_message(message.chat.id, response)
+
+
+bot.polling(none_stop=True)
